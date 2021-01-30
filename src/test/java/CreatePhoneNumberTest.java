5c9d8d8d18b5624810936ffce9a4c9a0ff57a692
Dorota Miler <dorota.miler7@gmail.com>
2021-01-30 23:14:51 +0100

    first test-to be corrected in lines with applied class

---
+++ src/test/java/CreatePhoneNumberTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreatePhoneNumberTest {
+    public class PhoneExampleTests {
+        @Test
+        public void tests() {
+            assertEquals("(123) 456-7890", Kata.createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+        }
+    }
+}
