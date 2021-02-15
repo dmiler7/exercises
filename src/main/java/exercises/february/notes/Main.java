5db40ec6131974523aa4262e62237786390e78cf
Dorota Miler <dorota.miler7@gmail.com>
2021-02-15 23:31:16 +0100

    method adding Stacks elements and use it in Main
    afterwards check if filled in Stack empty - false therefore methods work fine - correct

--- src/main/java/exercises/february/notes/Main.java
+++ src/main/java/exercises/february/notes/Main.java
@@ -2,12 +2,14 @@ package exercises.february.notes;
 
 import java.util.Stack;
 
-import static exercises.february.notes.StackEx.createStack;
+import static exercises.february.notes.StackEx.*;
 
 public class Main {
 
-    public static void main(String[]args){
-        Stack stk=createStack();
-        System.out.println("Check if stk is empty: "+stk.isEmpty());
+    public static void main(String[] args) {
+        Stack stk = createStack();
+        System.out.println("Check if stk is empty: " + isStackEmpty(stk));
+        System.out.println("Print Stack's elements: " + returnElements(stk));
+        System.out.println("Check ifnow stk is empty: " + isStackEmpty(stk));
     }
 }
--- src/main/java/exercises/february/notes/StackEx.java
+++ src/main/java/exercises/february/notes/StackEx.java
@@ -4,10 +4,19 @@ import java.util.Stack;
 
 public class StackEx {
 
-    public static Stack createStack(){
-        Stack stk=new Stack();
+    public static Stack createStack() {
+        Stack stk = new Stack();
         return stk;
     }
 
+    public static boolean isStackEmpty(Stack stk) {
+        return stk.isEmpty();
+    }
 
+    public static Stack returnElements(Stack stk) {
+        stk.push(78);
+        stk.push(1);
+        stk.push(4);
+        return stk;
+    }
 }
